a3023ef8536ab0f2ed33c65c62fdfa42a84776d3
malik <malikimenov@gmail.com>
2025-02-23 14:58:14 +0600

    [Add] import LoggerModule use MiddlewareConsumer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { HistoryPaymentModule } from './modules/history-payments/history.payment.module';
 import { PaymentModule } from './modules/payment/payment.module';
-// import { DatabaseModule } from './modules/database/database.module';
+import { DatabaseModule } from './modules/database/database.module';
+import { LoggerModule } from './helpers/logger/logger.module';
+import { DefMiddleware } from './middlewares/default.middleware';
 
 @Module({
   imports: [
     HistoryPaymentModule,
     PaymentModule,
-    // DatabaseModule
+    DatabaseModule,
+    LoggerModule
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(DefMiddleware).forRoutes('*');
+  }
+}
