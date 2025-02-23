78c4f3b9c3525f1433461d36395048989f763f0b
malik <malikimenov@gmail.com>
2025-02-23 15:31:46 +0600

    [Add] base method

--- src/modules/history-payments/history.payment.controller.ts
+++ src/modules/history-payments/history.payment.controller.ts
@@ -1,9 +1,57 @@
 import {
-    Controller,
-  } from '@nestjs/common';
+  Controller,
+  Get,
+  Param,
+  Query,
+  Req,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { RefId } from 'src/decorators/ref.decorator';
+import { CustomLogger } from 'src/helpers/logger/logger.service';
+import { IResponse } from 'src/helpers/types/response.type';
+import { HistoryPaymentService } from './history.payment.service';
+import { CustomRequest } from 'src/decorators/check-user';
 
 @Controller('history-payment')
 export class HistoryPaymentController {
-    constructor(
-      ) {}
+  constructor(
+    private readonly logger: CustomLogger,
+    private readonly historyPaymentService: HistoryPaymentService,
+  ) { }
+
+  @Get(':id')
+  @UsePipes(new ValidationPipe({ transform: true }))
+  async userOperationHistoryController(
+    @RefId() refId: string,
+    @Req() req: CustomRequest,
+    @Param('id') id: number,
+    @Query() query: any
+  ): Promise<IResponse<any>> {
+    this.logger.info(`
+        [START] userOperationHistoryController query: ${JSON.stringify(query)}`,
+      refId
+    );
+    try {
+      this.logger.info(`
+          [SUCCESS] userOperationHistoryController `,
+        refId
+      );
+
+      await this.historyPaymentService.getOperationUser(
+        query, 
+        refId
+      );
+      return {
+        success: true,
+        code: 200
+      }
+    } catch (error) {
+      this.logger.error(`
+          [Error] userOperationHistoryController error: ${JSON.stringify(error)}`,
+        refId
+      );
+      throw error;
+    }
+  }
 }
--- src/modules/history-payments/history.payment.service.ts
+++ src/modules/history-payments/history.payment.service.ts
@@ -1,7 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { CustomLogger } from 'src/helpers/logger/logger.service';
+import { HistoryPaymentRepository } from '../database/repository/history.payment.repository';
 
 @Injectable()
 export class HistoryPaymentService {
   constructor(
+        private readonly logger: CustomLogger,
+        private readonly historyPaymentRepository: HistoryPaymentRepository,
   ) {}
+
+  async getOperationUser(
+    query: any, 
+    refId: string
+  ){
+    try {
+      
+    } catch (error) {
+      throw error
+    }
+  }
 }
