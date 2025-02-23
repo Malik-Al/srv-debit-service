ba0495e97b89a404799c2438d84fa8d864d72b29
malik <malikimenov@gmail.com>
2025-02-23 14:58:56 +0600

    [Add] config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import config from '../msdata/configs/config.default.json'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(config.main.port ?? 3000);
 }
 bootstrap();
