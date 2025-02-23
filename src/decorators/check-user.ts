7d2eeab8ffcebcd1480bde8aacc742bbee960a3d
malik <malikimenov@gmail.com>
2025-02-23 23:45:36 +0600

    [Change] CheckUserGuard

--- src/decorators/check-user.ts
+++ src/decorators/check-user.ts
@@ -1,4 +1,4 @@
-import { CanActivate, ExecutionContext, Injectable, NotFoundException } from '@nestjs/common';
+import { CanActivate, ExecutionContext, HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { UserRepository } from 'src/modules/database/repository/user.repository';
 import { Request } from 'express';
 import uuid from 'uuid-base62';
@@ -10,25 +10,50 @@ export interface CustomRequest extends Request {
 
 @Injectable()
 export class CheckUserGuard implements CanActivate {
-    constructor(private readonly userRepository: UserRepository) {}
+    constructor(private readonly userRepository: UserRepository) { }
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
         const request = context.switchToHttp().getRequest<CustomRequest>();
-        const { user_id } = request.body;
 
+        if (request.method === 'POST' && !request.body) {
+            throw new HttpException(
+                {
+                    code: 400,
+                    success: false,
+                    message: 'fields is required',
+                },
+                400,
+            );
+        }
+
+        const { user_id } = request.body || { user_id: Number(request.params.id) }
         const refId: string = request.headers['reference-id'] || uuid.v4();
 
         if (!user_id) {
-            throw new NotFoundException('user_id is required');
+            throw new HttpException(
+                {
+                    code: 404,
+                    success: false,
+                    message: 'userId is required',
+                },
+                404,
+            );
         }
 
         const user = await this.userRepository.findByValue({ user_id }, refId);
 
         if (!user) {
-            throw new NotFoundException(`User with ID ${user_id} not found`);
+            throw new HttpException(
+                {
+                    code: 404,
+                    success: false,
+                    message: `User with ID ${user_id} not found`,
+                },
+                404,
+            );
         }
 
-        request.user = user;
+        (request as CustomRequest).user = user;
         return true;
     }
 }
